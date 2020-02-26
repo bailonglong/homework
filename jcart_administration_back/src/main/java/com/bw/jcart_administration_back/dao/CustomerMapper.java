1ad54ddd8f3e5a04138d9dd735c2985e847381c9
longlong.bai <longlong.bai@aliyun.com>
2020-02-26 16:29:41 +0800

    提交 dao

---
+++ jcart_administration_back/src/main/java/com/bw/jcart_administration_back/dao/CustomerMapper.java
@@ -0,0 +1,17 @@
+package com.bw.jcart_administration_back.dao;
+
+import io.cjf.jcartadministrationback.po.Customer;
+
+public interface CustomerMapper {
+    int deleteByPrimaryKey(Integer customerId);
+
+    int insert(Customer record);
+
+    int insertSelective(Customer record);
+
+    Customer selectByPrimaryKey(Integer customerId);
+
+    int updateByPrimaryKeySelective(Customer record);
+
+    int updateByPrimaryKey(Customer record);
+}
---
+++ jcart_administration_back/src/main/java/com/bw/jcart_administration_back/dao/OrderHistoryMapper.java
@@ -0,0 +1,17 @@
+package com.bw.jcart_administration_back.dao;
+
+import io.cjf.jcartadministrationback.po.OrderHistory;
+
+public interface OrderHistoryMapper {
+    int deleteByPrimaryKey(Long orderHistoryId);
+
+    int insert(OrderHistory record);
+
+    int insertSelective(OrderHistory record);
+
+    OrderHistory selectByPrimaryKey(Long orderHistoryId);
+
+    int updateByPrimaryKeySelective(OrderHistory record);
+
+    int updateByPrimaryKey(OrderHistory record);
+}
