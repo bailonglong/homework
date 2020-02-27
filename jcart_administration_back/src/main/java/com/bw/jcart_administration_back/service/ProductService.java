a2b850b8026f92c3d3c1fdef7f8487d5607a6151
longlong.bai <longlong.bai@aliyun.com>
2020-02-27 17:43:04 +0800

    service  and serviceImpl 提交

--- jcart_administration_back/src/main/java/com/bw/jcart_administration_back/service/ProductService.java
+++ jcart_administration_back/src/main/java/com/bw/jcart_administration_back/service/ProductService.java
@@ -1,4 +1,21 @@
 package com.bw.jcart_administration_back.service;
 
+import com.bw.jcart_administration_back.dto.in.ProductCreateInDTO;
+import com.bw.jcart_administration_back.dto.in.ProductUpdateInDTO;
+import com.bw.jcart_administration_back.dto.out.ProductListOutDTO;
+import com.bw.jcart_administration_back.dto.out.ProductShowOutDTO;
+import java.util.List;
+
 public interface ProductService {
+    Integer create(ProductCreateInDTO productCreateInDTO);
+
+    void update(ProductUpdateInDTO productUpdateInDTO);
+
+    void delete(Integer productId);
+
+    void batchDelete(List<Integer> productIds);
+
+//    Page<ProductListOutDTO> search(Integer pageNum);
+
+    ProductShowOutDTO getById(Integer productId);
 }
--- jcart_administration_back/src/main/java/com/bw/jcart_administration_back/service/impl/ProductServiceImpl.java
+++ jcart_administration_back/src/main/java/com/bw/jcart_administration_back/service/impl/ProductServiceImpl.java
@@ -1,4 +1,115 @@
 package com.bw.jcart_administration_back.service.impl;
 
-public class ProductServiceImpl {
+import com.alibaba.fastjson.JSON;
+import com.bw.jcart_administration_back.dao.ProductDetailMapper;
+import com.bw.jcart_administration_back.dao.ProductMapper;
+import com.bw.jcart_administration_back.dto.in.ProductCreateInDTO;
+import com.bw.jcart_administration_back.dto.in.ProductUpdateInDTO;
+import com.bw.jcart_administration_back.dto.out.ProductShowOutDTO;
+import com.bw.jcart_administration_back.po.Product;
+import com.bw.jcart_administration_back.po.ProductDetail;
+import com.bw.jcart_administration_back.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Autowired
+    private ProductDetailMapper productDetailMapper;
+
+    @Override
+    public Integer create(ProductCreateInDTO productCreateInDTO) {
+        Product product = new Product();
+        product.setProductCode(productCreateInDTO.getProductCode());
+        product.setProductName(productCreateInDTO.getProductName());
+        product.setPrice(productCreateInDTO.getPrice());
+        product.setDiscount(productCreateInDTO.getDiscount());
+        product.setStockQuantity(productCreateInDTO.getStockQuantity());
+        product.setStatus(productCreateInDTO.getStatus());
+        product.setMainPicUrl(productCreateInDTO.getMainPicUrl());
+        product.setRewordPoints(productCreateInDTO.getRewordPoints());
+        product.setSortOrder(productCreateInDTO.getSortOrder());
+        String description = productCreateInDTO.getDescription();
+        String productAbstract = description.substring(0, Math.min(100, description.length()));
+        product.setProductAbstract(productAbstract);
+        productMapper.insertSelective(product);
+
+        Integer productId = product.getProductId();
+        ProductDetail productDetail = new ProductDetail();
+        productDetail.setProductId(productId);
+        productDetail.setDescription(productCreateInDTO.getDescription());
+        List<String> otherPicUrls = productCreateInDTO.getOtherPicUrls();
+        productDetail.setOtherPicUrls(JSON.toJSONString(otherPicUrls));
+        productDetailMapper.insertSelective(productDetail);
+
+
+        return productId;
+    }
+
+    @Override
+    public void update(ProductUpdateInDTO productUpdateInDTO) {
+        Product product = new Product();
+        product.setProductId(productUpdateInDTO.getProductId());
+        product.setProductName(productUpdateInDTO.getProductName());
+        product.setPrice(productUpdateInDTO.getPrice());
+        product.setDiscount(productUpdateInDTO.getDiscount());
+        product.setStockQuantity(productUpdateInDTO.getStockQuantity());
+        product.setMainPicUrl(productUpdateInDTO.getMainPicUrl());
+        product.setStatus(productUpdateInDTO.getStatus());
+        product.setRewordPoints(productUpdateInDTO.getRewordPoints());
+        product.setSortOrder(productUpdateInDTO.getSortOrder());
+        String description = productUpdateInDTO.getDescription();
+        String productAbstract = description.substring(0, Math.min(100, description.length()));
+        product.setProductAbstract(productAbstract);
+        productMapper.updateByPrimaryKeySelective(product);
+
+        ProductDetail productDetail = new ProductDetail();
+        productDetail.setProductId(productUpdateInDTO.getProductId());
+        productDetail.setDescription(productUpdateInDTO.getDescription());
+        List<String> otherPicUrls = productUpdateInDTO.getOtherPicUrls();
+        productDetail.setOtherPicUrls(JSON.toJSONString(otherPicUrls));
+        productDetailMapper.updateByPrimaryKeySelective(productDetail);
+
+    }
+
+    @Override
+    public void delete(Integer productId) {
+
+        productMapper.deleteByPrimaryKey(productId);
+        productDetailMapper.deleteByPrimaryKey(productId);
+    }
+
+    @Override
+    public void batchDelete(List<Integer> productIds) {
+        productMapper.batchDelete(productIds);
+        productDetailMapper.batchDelete(productIds);
+    }
+
+    @Override
+    public ProductShowOutDTO getById(Integer productId) {
+        Product product = productMapper.selectByPrimaryKey(productId);
+        ProductDetail productDetail = productDetailMapper.selectByPrimaryKey(productId);
+
+        ProductShowOutDTO productShowOutDTO = new ProductShowOutDTO();
+        productShowOutDTO.setProductId(productId);
+        productShowOutDTO.setProductCode(product.getProductCode());
+        productShowOutDTO.setProductName(product.getProductName());
+        productShowOutDTO.setPrice(product.getPrice());
+        productShowOutDTO.setDiscount(product.getDiscount());
+        productShowOutDTO.setStatus(product.getStatus());
+        productShowOutDTO.setMainPicUrl(product.getMainPicUrl());
+        productShowOutDTO.setRewordPoints(product.getRewordPoints());
+        productShowOutDTO.setSortOrder(product.getSortOrder());
+        productShowOutDTO.setStockQuantity(product.getStockQuantity());
+
+        productShowOutDTO.setDescription(productDetail.getDescription());
+        String otherPicUrlsJson = productDetail.getOtherPicUrls();
+        List<String> otherPicUrls = JSON.parseArray(otherPicUrlsJson, String.class);
+        productShowOutDTO.setOtherPicUrls(otherPicUrls);
+
+        return productShowOutDTO;
+    }
 }
