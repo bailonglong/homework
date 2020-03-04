e3f8e04a776a34ae3c509325bab9752796f6716b
longlong.bai <longlong.bai@aliyun.com>
2020-03-04 19:39:28 +0800

    submit orderServiceImpl

--- jcart_administration_back/src/main/java/com/bw/jcart_administration_back/controller/OrderController.java
+++ jcart_administration_back/src/main/java/com/bw/jcart_administration_back/controller/OrderController.java
@@ -1,14 +1,9 @@
 package com.bw.jcart_administration_back.controller;
 
 import com.bw.jcart_administration_back.dto.in.OrderCheckoutInDTO;
-import com.bw.jcart_administration_back.dto.in.OrderProductInDTO;
 import com.bw.jcart_administration_back.dto.in.OrderSearchInDTO;
 import com.bw.jcart_administration_back.dto.out.*;
-import com.bw.jcart_administration_back.enumeration.OrderStatus;
-import com.bw.jcart_administration_back.po.Order;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -23,16 +18,7 @@ public class OrderController {
     @PostMapping("/checkout")
     public Long checkout(@RequestBody OrderCheckoutInDTO orderCheckoutInDTO,
                         @RequestAttribute Integer customerId){
-        List<OrderProductInDTO> orderProducts = orderCheckoutInDTO.getOrderProducts();
-        orderProducts.stream().map(orderProduct -> {
-            Integer productId = orderProduct.getProductId();
 
-        })
-
-
-        Order order = new Order();
-        order.setCustomerId(customerId);
-        order.setStatus((byte) OrderStatus.ToProcess.ordinal());
         return null;
     }
 
--- jcart_administration_back/src/main/java/com/bw/jcart_administration_back/service/impl/OrderServiceImpl.java
+++ jcart_administration_back/src/main/java/com/bw/jcart_administration_back/service/impl/OrderServiceImpl.java
@@ -3,9 +3,19 @@ package com.bw.jcart_administration_back.service.impl;
 import com.bw.jcart_administration_back.dao.OrderDetailMapper;
 import com.bw.jcart_administration_back.dao.OrderMapper;
 import com.bw.jcart_administration_back.dto.in.OrderCheckoutInDTO;
+import com.bw.jcart_administration_back.dto.in.OrderProductInDTO;
+import com.bw.jcart_administration_back.dto.out.ProductShowOutDTO;
+import com.bw.jcart_administration_back.enumeration.OrderStatus;
+import com.bw.jcart_administration_back.po.Order;
 import com.bw.jcart_administration_back.service.OrderService;
+import com.bw.jcart_administration_back.service.ProductService;
+import com.bw.jcart_administration_back.vo.OrderProductVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -13,8 +23,40 @@ public class OrderServiceImpl implements OrderService {
     private OrderMapper orderMapper;
     @Autowired
     private OrderDetailMapper orderDetailMapper;
+    @Autowired
+    private ProductService  productService;
     @Override
     public Integer createout(OrderCheckoutInDTO orderCheckoutInDTO,Integer customerId) {
+        List<OrderProductInDTO> orderProducts = orderCheckoutInDTO.getOrderProducts();
+        List<OrderProductVo> vos = orderProducts.stream().map(orderProduct -> {
+            ProductShowOutDTO orderProdu = productService.getById(orderProduct.getProductId());
+            OrderProductVo orderProductVo = new OrderProductVo();
+            orderProductVo.setProductId(orderProdu.getProductId());
+            orderProductVo.setQuantity(orderProduct.getQuantity());
+            Double unitPrice = orderProdu.getPrice() * orderProdu.getDiscount();
+            orderProductVo.setUnitPrice(unitPrice);
+            Double totalPrice = unitPrice * orderProduct.getQuantity();
+            orderProductVo.setTotalPrice(totalPrice);
+            Integer unitRewordPoins = orderProdu.getRewordPoints();
+            orderProductVo.setUnitRewordPoints(unitRewordPoins);
+            Integer totalRewordPoints = orderProdu.getRewordPoints();
+            orderProductVo.setUnitRewordPoints(totalRewordPoints);
+
+            return orderProductVo;
+        }).collect(Collectors.toList());
+
+        double AllTotalPrice = vos.stream().mapToDouble(p -> p.getTotalPrice()).sum();
+        int sum = vos.stream().mapToInt(p -> p.getTotalRewordPoins()).sum();
+
+        Order order = new Order();
+        order.setCustomerId(customerId);
+        order.setStatus((byte) OrderStatus.ToProcess.ordinal());
+        order.setTotalPrice(AllTotalPrice);
+        order.setRewordPoints(sum);
+        Date date = new Date();
+        order.setCreateTime(date);
+        order.setUpdateTime(date);
         return null;
     }
+
 }
